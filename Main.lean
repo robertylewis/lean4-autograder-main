3b9e9312476df15845ac4af6d1959b332f8b359f
jrr6 <7482866+jrr6@users.noreply.github.com>
2023-08-28 19:59:03 -0400

    Start debugging importModules

--- Main.lean
+++ Main.lean
@@ -62,6 +62,127 @@ def gradeSubmission (sheetName : Name) (sheet submission : Environment) : IO (Ar
     throw <| IO.userError "There are no exercises annotated with points in the template; thus, the submission can't be graded."
   return results
 
+private def mkInitialExtensionStates : IO (Array EnvExtensionState) := EnvExtensionInterfaceImp.mkInitialExtStates
+
+private def mkExtNameMap (startingAt : Nat) : IO (HashMap Name Nat) := do
+  let descrs ← persistentEnvExtensionsRef.get
+  let mut result := {}
+  for h : i in [startingAt : descrs.size] do
+    have : i < descrs.size := h.upper
+    let descr := descrs[i]
+    result := result.insert descr.name i
+  return result
+
+
+private def setImportedEntries (env : Environment) (mods : Array ModuleData) (startingAt : Nat := 0) : IO Environment := do
+  let mut env := env
+  let extDescrs ← persistentEnvExtensionsRef.get
+  /- For extensions starting at `startingAt`, ensure their `importedEntries` array have size `mods.size`. -/
+  for extDescr in extDescrs[startingAt:] do
+    env := extDescr.toEnvExtension.modifyState env fun s => { s with importedEntries := mkArray mods.size #[] }
+  /- For each module `mod`, and `mod.entries`, if the extension name is one of the extensions after `startingAt`, set `entries` -/
+  let extNameIdx ← mkExtNameMap startingAt
+  for h : modIdx in [:mods.size] do
+    have : modIdx < mods.size := h.upper
+    let mod := mods[modIdx]
+    for (extName, entries) in mod.entries do
+      if let some entryIdx := extNameIdx.find? extName then
+        env := extDescrs[entryIdx]!.toEnvExtension.modifyState env fun s => { s with importedEntries := s.importedEntries.set! modIdx entries }
+  return env
+
+private def ensureExtensionsArraySize (env : Environment) : IO Environment :=
+  EnvExtensionInterfaceImp.ensureExtensionsSize env
+
+private partial def finalizePersistentExtensions (env : Environment) (mods : Array ModuleData) (opts : Options) : IO Environment := do
+  loop 0 env
+where
+  loop (i : Nat) (env : Environment) : IO Environment := do
+    -- Recall that the size of the array stored `persistentEnvExtensionRef` may increase when we import user-defined environment extensions.
+    let pExtDescrs ← persistentEnvExtensionsRef.get
+    if i < pExtDescrs.size then
+      let extDescr := pExtDescrs[i]!
+      let s := extDescr.toEnvExtension.getState env
+      let prevSize := (← persistentEnvExtensionsRef.get).size
+      let prevAttrSize ← getNumBuiltinAttributes
+      let newState ← extDescr.addImportedFn s.importedEntries { env := env, opts := opts }
+      let mut env := extDescr.toEnvExtension.setState env { s with state := newState }
+      env ← ensureExtensionsArraySize env
+      if (← persistentEnvExtensionsRef.get).size > prevSize || (← getNumBuiltinAttributes) > prevAttrSize then
+        -- This branch is executed when `pExtDescrs[i]` is the extension associated with the `init` attribute, and
+        -- a user-defined persistent extension is imported.
+        -- Thus, we invoke `setImportedEntries` to update the array `importedEntries` with the entries for the new extensions.
+        env ← setImportedEntries env mods prevSize
+        -- See comment at `updateEnvAttributesRef`
+        env ← updateEnvAttributes env
+      loop (i + 1) env
+    else
+      return env
+
+partial def myImportModules (imports : List Import) (opts : Options) (trustLevel : UInt32 := 0) : IO Environment := profileitIO "import" opts do
+  for imp in imports do
+    if imp.module matches .anonymous then
+      throw <| IO.userError "import failed, trying to import module with anonymous name"
+  withImporting do
+    let (_, s) ← importMods imports |>.run {}
+    let mut numConsts := 0
+    for mod in s.moduleData do
+      numConsts := numConsts + mod.constants.size + mod.extraConstNames.size
+    let mut modIdx : Nat := 0
+    let mut const2ModIdx : HashMap Name ModuleIdx := mkHashMap (capacity := numConsts)
+    let mut constantMap : HashMap Name ConstantInfo := mkHashMap (capacity := numConsts)
+    for mod in s.moduleData do
+      for cname in mod.constNames, cinfo in mod.constants do
+        match constantMap.insert' cname cinfo with
+        | (constantMap', replaced) =>
+          constantMap := constantMap'
+          if replaced then
+            throwAlreadyImported s const2ModIdx modIdx cname
+        const2ModIdx := const2ModIdx.insert cname modIdx
+      for cname in mod.extraConstNames do
+        const2ModIdx := const2ModIdx.insert cname modIdx
+      modIdx := modIdx + 1
+    let constants : ConstMap := SMap.fromHashMap constantMap false
+    let exts ← mkInitialExtensionStates
+    IO.println "make initial states"
+    let env : Environment := {
+      const2ModIdx    := const2ModIdx
+      constants       := constants
+      extraConstNames := {}
+      extensions      := exts
+      header          := {
+        quotInit     := !imports.isEmpty -- We assume `core.lean` initializes quotient module
+        trustLevel   := trustLevel
+        imports      := imports.toArray
+        regions      := s.regions
+        moduleNames  := s.moduleNames
+        moduleData   := s.moduleData
+      }
+    }
+    let env ← setImportedEntries env s.moduleData
+    IO.println "set imported entries"
+    let env ← finalizePersistentExtensions env s.moduleData opts
+    IO.println "finalize persistent"
+    pure env
+where
+  importMods : List Import → StateRefT ImportState IO Unit
+  | []    => pure ()
+  | i::is => do
+    if i.runtimeOnly || (← get).moduleNameSet.contains i.module then
+      importMods is
+    else do
+      modify fun s => { s with moduleNameSet := s.moduleNameSet.insert i.module }
+      let mFile ← findOLean i.module
+      unless (← mFile.pathExists) do
+        throw <| IO.userError s!"object file '{mFile}' of module {i.module} does not exist"
+      let (mod, region) ← readModuleData mFile
+      importMods mod.imports.toList
+      modify fun s => { s with
+        moduleData  := s.moduleData.push mod
+        regions     := s.regions.push region
+        moduleNames := s.moduleNames.push i.module
+      }
+      importMods is
+
 def main (args : List String) : IO Unit := do
   let usage := throw <| IO.userError s!"Usage: autograder Exercise.Sheet.Module submission-file.lean"
   let [sheetName, submission] := args | usage
@@ -95,13 +216,8 @@ def main (args : List String) : IO Unit := do
       IO.println "Compilation succeedded"
       searchPathRef.modify fun sp => submissionBuildDir :: sp
       IO.println "Modified search path"
-      try
-        let env ← importModules [{module := `Submission}] {}
-      catch ex =>
-        IO.println "Import Error!"
       -- FIXME: silently crashing on `importModules`
-      -- importModules [{module := `Submission}] {}
-      importModules [] {}
+      myImportModules [{module := `Submission}] {}
     catch ex =>
       IO.println "Unexpected error encountered; pushing to errors..."
       errors := errors.push ex.toString
