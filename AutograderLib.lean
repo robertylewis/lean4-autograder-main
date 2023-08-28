d54aef625389307ec9ec4dc67713c53585d46b55
jrr6 <7482866+jrr6@users.noreply.github.com>
2023-08-28 16:05:03 -0400

    Account for plurality in attribute

--- AutograderLib.lean
+++ AutograderLib.lean
@@ -4,9 +4,12 @@ open Lean
 
 -- Attribute for point values
 declare_syntax_cat ptVal
+declare_syntax_cat ptWord
 syntax num : ptVal
 syntax scientific : ptVal
-syntax (name := problem) ptVal "points" : attr
+syntax "point" : ptWord
+syntax "points" : ptWord
+syntax (name := problem) ptVal ptWord : attr
 
 initialize problemAttr : ParametricAttribute Float ←
   registerParametricAttribute {
@@ -14,10 +17,11 @@ initialize problemAttr : ParametricAttribute Float ←
     descr := "specify a point value of a problem"
     -- applicationTime := .afterCompilation
     getParam := λ _ stx => match stx with
-      | `(attr| $pts:scientific points) => 
+      | `(attr| $pts:scientific point) | `(attr| $pts:scientific points) => 
         let (n, s, d) := pts.getScientific
         return Float.ofScientific n s d
-      | `(attr| $pts:num points) => return pts.getNat.toFloat
+      | `(attr| $pts:num point) | `(attr| $pts:num points) =>
+        return pts.getNat.toFloat
       | _  => throwError "invalid problem attribute"
     afterSet := λ _ _ => do pure ()
   }
