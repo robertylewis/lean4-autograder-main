dbc4090108697bb614f78c44c939b26c3aeb0e83
jrr6 <7482866+jrr6@users.noreply.github.com>
2023-08-31 18:23:11 -0400

    Extremely rudimentary first pass at using process

--- Main.lean
+++ Main.lean
@@ -157,36 +157,39 @@ def main : IO Unit := do
   let sheetName := s!"{solutionDirName}.{solutionModuleName}".toName
   searchPathRef.set (← addSearchPathFromEnv {})
   let sheet ← importModules [{module := sheetName}] {}
-  let submissionBuildDir : FilePath := "build" / "submission"
-  FS.createDirAll submissionBuildDir
-  let submissionOlean := submissionBuildDir / "Submission.olean"
-  if ← submissionOlean.pathExists then FS.removeFile submissionOlean
-  let mut errors := #[]
-  let submissionEnv ←
-    try
-      let out ← IO.Process.output {
-        cmd := "lean"
-        args := #[submissionFileName, "-o", submissionOlean.toString]
-      }
-      if out.exitCode != 0 then
-        let result : ExerciseResult := {
-            name := submissionFileName,
-            status := "failed",
-            output := out.stderr,
-            score := 0.0
-        }
-        let results : GradingResults := { tests := #[result] }
-        IO.FS.writeFile "../results/results.json" (toJson results).pretty
-        throw <| IO.userError s!"Lean exited with code {out.exitCode}:\n{out.stderr}"
-      searchPathRef.modify fun sp => submissionBuildDir :: sp
-      -- Is `importModules` silently failing? If so, it might be because the
-      -- autograder has not been allocated enough resources on Gradescope!
-      -- TODO: look into replacing with `process`
-      importModules [{module := `Submission}] {}
-    catch ex =>
-      errors := errors.push ex.toString
-      importModules sheet.header.imports.toList {}
+  let (submissionEnv, _) ← process "" (← mkEmptyEnvironment) {}
+  initSearchPath (← findSysroot)
+  -- let submissionBuildDir : FilePath := "build" / "submission"
+  -- FS.createDirAll submissionBuildDir
+  -- let submissionOlean := submissionBuildDir / "Submission.olean"
+  -- if ← submissionOlean.pathExists then FS.removeFile submissionOlean
+  -- let mut errors := #[]
+  -- let submissionEnv ←
+  --   try
+  --     let out ← IO.Process.output {
+  --       cmd := "lean"
+  --       args := #[submissionFileName, "-o", submissionOlean.toString]
+  --     }
+  --     if out.exitCode != 0 then
+  --       let result : ExerciseResult := {
+  --           name := submissionFileName,
+  --           status := "failed",
+  --           output := out.stderr,
+  --           score := 0.0
+  --       }
+  --       let results : GradingResults := { tests := #[result] }
+  --       IO.FS.writeFile "../results/results.json" (toJson results).pretty
+  --       throw <| IO.userError s!"Lean exited with code {out.exitCode}:\n{out.stderr}"
+  --     searchPathRef.modify fun sp => submissionBuildDir :: sp
+  --     -- Is `importModules` silently failing? If so, it might be because the
+  --     -- autograder has not been allocated enough resources on Gradescope!
+  --     -- TODO: look into replacing with `process`
+  --     -- importModules [{module := `Submission}] {}
+  --   catch ex =>
+  --     errors := errors.push ex.toString
+  --     importModules sheet.header.imports.toList {}
   let tests ← gradeSubmission sheetName sheet submissionEnv
   let results : GradingResults := { tests }
-  if errors.size == 0 then
-    IO.FS.writeFile "../results/results.json" (toJson results).pretty
+  IO.FS.writeFile "../results/results.json" (toJson results).pretty
+  -- if errors.size == 0 then
+  --   IO.FS.writeFile "../results/results.json" (toJson results).pretty
